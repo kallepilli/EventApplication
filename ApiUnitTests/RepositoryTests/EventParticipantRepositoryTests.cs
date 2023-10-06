74cea9d2f96485905b6eba8e2d83ae397c9422a9
kapill <kapill@ttu.ee>
2023-10-06 23:10:56 +0300

    Add repository tests

---
+++ ApiUnitTests/RepositoryTests/EventParticipantRepositoryTests.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using webapi.Data.Model;
+using webapi.Repositories.Interfaces;
+
+namespace ApiUnitTests.RepositoryTests
+{
+    [TestClass]
+    public class EventParticipantRepositoryTests : BaseRepositoryTests<EventParticipant>
+    {
+        protected override EventParticipant GetTestItem()
+        {
+            return new EventParticipant
+            {
+                EventId = "test1",
+                ParticipantId = "testParticipant1",
+                AdditionalInfo = "Sooviks hotellituba",
+                PaymentMethod = PaymentMethod.Cash
+            };
+        }
+
+        protected override EventParticipant GetUpdatedItem()
+        {
+            return new EventParticipant
+            {
+                EventId = "test1",
+                ParticipantId = "testParticipant1",
+                AdditionalInfo = "Sooviks hotellituba Updated",
+                PaymentMethod = PaymentMethod.BankTransfer
+            };
+        }
+
+        protected override string GetNonExistingId()
+        {
+            return "999";
+        }
+
+        protected override List<EventParticipant> GetTestItems()
+        {
+            return new List<EventParticipant>
+            {
+                new EventParticipant
+                {
+                    EventId = "test1",
+                    ParticipantId = "testParticipant1",
+                    AdditionalInfo = "Sooviks hotellituba",
+                    PaymentMethod = PaymentMethod.Cash
+                },
+                new EventParticipant
+                {
+                    EventId = "test1",
+                    ParticipantId = "testParticipant2",
+                    AdditionalInfo = "",
+                    PaymentMethod = PaymentMethod.BankTransfer
+                }
+            };
+        }
+
+        protected override Func<EventParticipant, string> GetIdFunc() => e => e.Id;
+
+        [TestMethod]
+        public void GetEventParticipantListByEventId_ReturnsListOfParticipants()
+        {
+            var eventId = "test1";
+            var participants = GetTestItems();
+
+            var mockEventParticipantRepository = new Mock<IEventParticipantRepository<EventParticipant>>();
+            var eventParticipantRepository = mockEventParticipantRepository.Object;
+
+            mockEventParticipantRepository.Setup(r => r.GetEventParticipantListByEventId(eventId)).Returns(participants);
+
+            var result = eventParticipantRepository.GetEventParticipantListByEventId(eventId);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(participants.Count, result.Count);
+        }
+    }
+}
---
+++ ApiUnitTests/RepositoryTests/ParticipantRepositoryTests.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using webapi.Data.Model;
+using webapi.Repositories.Interfaces;
+
+namespace ApiUnitTests.RepositoryTests
+{
+    [TestClass]
+    public class ParticipantRepositoryTests : BaseRepositoryTests<Participant>
+    {
+        protected override Participant GetTestItem()
+        {
+            return new Participant
+            {
+                Id = "testParticipant1",
+                IsCompany = false,
+                FirstName = "Kalle",
+                LastName = "Pilli",
+                IdCode = "39208014225"
+            };
+        }
+
+        protected override Participant GetUpdatedItem()
+        {
+            return new Participant
+            {
+                Id = "testParticipant1",
+                IsCompany = false,
+                FirstName = "Kalle Updated",
+                LastName = "Pilli UpdTaed",
+                IdCode = "39208014225"
+            };
+        }
+
+        protected override string GetNonExistingId()
+        {
+            return "999";
+        }
+
+        protected override List<Participant> GetTestItems()
+        {
+            return new List<Participant>
+            {
+                new Participant
+                {
+                    Id = "testParticipant1",
+                    IsCompany = false,
+                    FirstName = "Kalle",
+                    LastName = "Pilli",
+                    IdCode = "39208014225"
+                },
+                new Participant
+                {
+                    Id = "testParticipant2",
+                    IsCompany = false,
+                    FirstName = "Investor",
+                    LastName = "Toomas",
+                    IdCode = "36308010071"
+                }
+            };
+        }
+
+        protected override Func<Participant, string> GetIdFunc() => e => e.Id;
+
+        [TestMethod]
+        public void IsIdCodeAvailable_ReturnsTrue()
+        {
+            var idCode = "testIdCode";
+            var participants = GetTestItems();
+
+            var mockParticipantRepository = new Mock<IParticipantRepository<Participant>>();
+            var participantRepository = mockParticipantRepository.Object;
+
+            mockParticipantRepository.Setup(r => r.IsIdCodeAvailable(idCode)).Returns(true);
+
+            var result = participantRepository.IsIdCodeAvailable(idCode);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IsIdCodeAvailable_ReturnsFalse()
+        {
+            var idCode = "testIdCode";
+            var participants = GetTestItems();
+
+            var mockParticipantRepository = new Mock<IParticipantRepository<Participant>>();
+            var participantRepository = mockParticipantRepository.Object;
+
+            mockParticipantRepository.Setup(r => r.IsIdCodeAvailable(idCode)).Returns(false);
+
+            var result = participantRepository.IsIdCodeAvailable(idCode);
+
+            Assert.IsFalse(result);
+        }
+    }
+}
